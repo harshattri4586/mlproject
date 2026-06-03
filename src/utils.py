6ff6cc8a72dd8f197e3341d51d7103299bb7929f
harshattri4586 <harshzaat@gmail.com>
2026-06-03 07:34:49 +0530

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,9 @@ import dill
 
 import numpy as np
 import pandas as pd
+
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -23,21 +25,23 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
-            # Train the model
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            # Predicting the output
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
-            # Calculate model score 
             train_model_score = r2_score(y_train,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
 
